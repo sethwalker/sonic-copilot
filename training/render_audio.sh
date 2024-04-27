71917a8db814d48f3dc3e1d19df7372fa66bef4a
Brock Wilcox <awwaiid@thelackthereof.org>
2024-04-26 20:26:56 -0400

    Skip existing files; decrease duration

--- training/render_audio.sh
+++ training/render_audio.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 startup_shutdown_duration=5
-play_duration=60
+play_duration=10
 
 for source_file in data/**/*.rb ; do
-  echo "Rendering $source_file"
   destination_file=${source_file}.wav
-  ((sleep $play_duration && echo) | sonic-pi-tool record $PWD/$destination_file) &
-  echo "Waiting for recording to start"
-  sleep $startup_shutdown_duration
-  echo "Evaluating $source_file"
-  sonic-pi-tool eval-file $source_file
-  echo "Waiting for $play_duration"
-  sleep $play_duration
-  echo "Stopping"
-  sonic-pi-tool stop
-  sleep $startup_shutdown_duration
+  if [ ! -f $destination_file ]; then
+    echo "Rendering $source_file"
+    ((sleep $play_duration && echo) | sonic-pi-tool record $PWD/$destination_file) &
+    echo "Waiting for recording to start"
+    sleep $startup_shutdown_duration
+    echo "Evaluating $source_file"
+    sonic-pi-tool eval-file $source_file
+    echo "Waiting for $play_duration"
+    sleep $play_duration
+    echo "Stopping"
+    sonic-pi-tool stop
+    sleep $startup_shutdown_duration
+  else
+    echo "Skipping $source_file, already rendered"
+  fi
 done
